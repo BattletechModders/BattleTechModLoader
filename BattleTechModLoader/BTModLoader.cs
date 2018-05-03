efa654c92a962e04f3afa36b02bc5b4b78d36935
Michael P. Starkweather <mpstark@gmail.com>
2018-05-03 16:43:43 -0500

    Add project files.

---
+++ BattleTechModLoader/BTModLoader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using BattleTech;
+using System.Reflection;
+using Harmony;
+
+namespace BattleTechModLoader
+{
+    public static class BTModLoader
+    {
+        public static string LogPath;
+        public static string ModDirectory;
+
+        public static void LoadDLL(string path, StreamWriter logWriter = null, string methodName = "Init", string typeName = null, BindingFlags bFlags = BindingFlags.Public | BindingFlags.Static)
+        {
+            var fileName = Path.GetFileName(path);
+            
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(path);
+                List<Type> types = new List<Type>();
+
+                // find the type/s with our entry point/s
+                if (typeName == null)
+                {
+                    types.AddRange(assembly.GetTypes().Where(x => x.GetMethod(methodName, bFlags) != null));
+                }
+                else
+                {
+                    types.Add(assembly.GetType(typeName));
+                }
+
+                if (types.Count > 0)
+                {
+                    // run each entry point
+                    foreach (var type in types)
+                    {
+                        var entryMethod = type.GetMethod(methodName, bFlags);
+                        entryMethod.Invoke(null, null);
+                        logWriter?.WriteLine("{0}: Found and called {1}.{2}", fileName, type.Name, entryMethod.Name);
+                    }
+                }
+                else
+                {
+                    logWriter?.WriteLine("{0}: Failed to find specified entry point: {1}{2}", fileName, (typeName == null) ? typeName : "NotSpecified", methodName);
+                }
+            }
+            catch (Exception e)
+            {
+                logWriter?.WriteLine("{0}: While loading a dll, an exception occured: {1}", fileName, e.ToString());
+            }
+        }
+
+        public static void Init()
+        {
+            ModDirectory = Path.Combine(Path.GetDirectoryName(VersionManifestUtilities.MANIFEST_FILEPATH), @"..\..\..\Mods\");
+            LogPath = Path.Combine(ModDirectory, "BTModLoader.log");
+
+            if (!Directory.Exists(ModDirectory))
+                Directory.CreateDirectory(ModDirectory);
+
+            var harmony = HarmonyInstance.Create("io.github.mpstark.BTModLoader");
+            
+            using (var logWriter = File.CreateText(LogPath))
+            {
+                logWriter.WriteLine("BTModLoader -- {0}", DateTime.Now.ToString());
+
+                var dllPaths = new DirectoryInfo(ModDirectory).GetFiles("*.dll", SearchOption.TopDirectoryOnly);
+                if (dllPaths.Length == 0)
+                {
+                    logWriter.WriteLine(@"No .dlls loaded. DLLs must be placed in the root of the folder \BATTLETECH\Mods\.");
+                }
+                else
+                {
+                    foreach (var dllFileInfo in dllPaths)
+                    {
+                        logWriter.WriteLine("Found DLL: {0}", dllFileInfo.Name);
+                        LoadDLL(dllFileInfo.ToString(), logWriter);
+                    }
+                }
+                
+                // print out harmony summary
+                logWriter.WriteLine("");
+                logWriter.WriteLine("Harmony Patched Methods (after mod loader startup):");
+                var patchedMethods = harmony.GetPatchedMethods();
+                foreach (var method in patchedMethods)
+                {
+                    var info = harmony.IsPatched(method);
+
+                    if (info != null)
+                    {
+                        logWriter.WriteLine("{0}.{1}.{2}:", method.ReflectedType.Namespace, method.ReflectedType.Name, method.Name);
+
+                        // prefixes
+                        if (info.Prefixes.Count != 0)
+                            logWriter.WriteLine("\tPrefixes:");
+                        foreach (var patch in info.Prefixes)
+                        {
+                            logWriter.WriteLine("\t\t{0}", patch.owner);
+                        }
+
+                        // transpilers
+                        if (info.Transpilers.Count != 0)
+                            logWriter.WriteLine("\tTranspilers:");
+                        foreach (var patch in info.Transpilers)
+                        {
+                            logWriter.WriteLine("\t\t{0}", patch.owner);
+                        }
+
+                        // postfixes
+                        if (info.Postfixes.Count != 0)
+                            logWriter.WriteLine("\tPostfixes:");
+                        foreach (var patch in info.Postfixes)
+                        {
+                            logWriter.WriteLine("\t\t{0}", patch.owner);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BattleTechModLoaderInjector/BTMLInjector.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace BattleTechModLoader
+{
+    public class BTMLInjector
+    {
+        static void Main(string[] args)
+        {
+            string directory = Directory.GetCurrentDirectory();
+
+            string injectedDLLFileName = "BattleTechModLoader.dll";
+            string injectType = "BattleTechModLoader.BTModLoader";
+            string injectMethod = "Init";
+
+            string gameDLLFileName = "Assembly-CSharp.dll";
+            string backupExt = ".orig";
+
+            string hookType = "BattleTech.GameInstance";
+            string hookMethod = ".ctor";
+            
+            string gameDLLPath = Path.Combine(directory, gameDLLFileName);
+            string gameDLLBackupPath = Path.Combine(directory, gameDLLFileName + backupExt);
+            string injectDLLPath = Path.Combine(directory, injectedDLLFileName);
+
+            Console.WriteLine("BattleTechModLoader Injector");
+            Console.WriteLine("----------------------------");
+
+            try
+            {
+                if (!IsInjected(gameDLLPath, hookType, hookMethod, injectDLLPath, injectType, injectMethod))
+                {
+                    Backup(gameDLLPath, gameDLLBackupPath);
+                    Inject(gameDLLPath, hookType, hookMethod, injectDLLPath, injectType, injectMethod);
+                }
+                else
+                {
+                    Console.WriteLine("{0} already injected with {1}.{2}.", gameDLLFileName, injectType, injectMethod);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception occured: {0}", e.ToString());
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        public static void Backup(string filePath, string backupFilePath)
+        {
+            if (File.Exists(backupFilePath))
+                File.Delete(backupFilePath);
+
+            File.Copy(filePath, backupFilePath);
+
+            Console.WriteLine("{0} backed up to {1}", Path.GetFileName(filePath), Path.GetFileName(backupFilePath));
+        }
+
+        public static void Inject(string hookFilePath, string hookType, string hookMethod, string injectFilePath, string injectType, string injectMethod)
+        {
+            Console.WriteLine("Injecting {0} with {1}.{2} at {3}.{4}", Path.GetFileName(hookFilePath), injectType, injectMethod, hookType, hookMethod);
+
+            using (var game = ModuleDefinition.ReadModule(hookFilePath, new ReaderParameters { ReadWrite = true }))
+            {
+                using (var injected = ModuleDefinition.ReadModule(injectFilePath))
+                {
+                    // get the methods that we're hooking and injecting
+                    var injectedMethod = injected.GetType(injectType).Methods.Single(x => x.Name == injectMethod);
+                    var hookedMethod = game.GetType(hookType).Methods.First(x => x.Name == hookMethod);
+
+                    // inject our method into the beginning of the hooks method
+                    hookedMethod.Body.GetILProcessor().InsertBefore(hookedMethod.Body.Instructions[0], Instruction.Create(OpCodes.Call, game.ImportReference(injectedMethod)));
+
+                    // save the modified assembly
+                    Console.WriteLine("Writing back to {0}...", Path.GetFileName(hookFilePath));
+                    game.Write();
+                    Console.WriteLine("Injection complete!");
+                }
+            }
+        }
+
+        public static bool IsInjected(string hookFilePath, string hookType, string hookMethod, string injectFilePath, string injectType, string injectMethod)
+        {
+            using (var game = ModuleDefinition.ReadModule(hookFilePath))
+            {
+                // get the methods that we're hooking and injecting
+                var hookedMethod = game.GetType(hookType).Methods.First(x => x.Name == hookMethod);
+
+                // check if we've been injected
+                foreach (var instruction in hookedMethod.Body.Instructions)
+                {
+                    if (instruction.OpCode.Equals(OpCodes.Call) && instruction.Operand.ToString().Equals(String.Format("System.Void {0}::{1}()", injectType, injectMethod)))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
