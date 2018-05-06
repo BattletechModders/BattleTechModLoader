41cc983c450f70a4c06220ce87f3bdfdd9970aaa
Michael P. Starkweather <mpstark@gmail.com>
2018-05-05 21:16:45 -0500

    Some cleanup of the VS project

--- BattleTechModLoader/Properties/AssemblyInfo.cs
+++ BattleTechModLoader/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BattleTechModLoader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple mod loader for HBS's BattleTech PC game.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BattleTechModLoader")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Public domain under the Unlicence")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.*")]
